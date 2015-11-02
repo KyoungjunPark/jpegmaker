5b980c04357748f5644805464a5f1550833695ca
Park Kyoungjun <pgj525@cau.ac.kr>
2015-11-02 21:27:02 +0900

    begin of FFTW3

--- JPEGMaker/Main.h
+++ JPEGMaker/Main.h
@@ -2,7 +2,7 @@
 /*       Computer Science Engineering, Chung-Ang University, Seoul Korea     */
 /*                                                                           */
 /*   Title      : Design a Converter between JPEG format and Gray scale image*/
-/*   File Name  : Main.cpp                                                   */
+/*   File Name  : Main.h                                                     */
 /*   Author     : Kyoung-Jun Park   pgj525@cau.ac.kr                         */
 /*   Date       : Nov 02, 2015                                               */
 /*   Version    :                                                            */
@@ -23,45 +23,12 @@
 /*                                                                           */
 /*****************************************************************************/
 
-#include "Main.h"
+#include <iostream>
+#include <fftw3.h>
 
-int main()
-{
-	CImg<unsigned char> image("hyojoo.jpeg");
-	//CImg<unsigned char> grayImage = RGBtoGrayScale(image);
-	CImg<unsigned char> grayImage("hyojoo_gray.jpeg");
+#include "CImg/CImg.h"
 
-	CImgDisplay main_disp;
-	
-	for(int i = 0; i < grayImage.dimx(); i++){
-		for(int j = 0; j < grayImage.dimy(); j++){
-			for(int k=0; k<3; k++){
-				grayImage(i,j,0,k) /= 2;
-				main_disp(grayImage, "Main display", 0);
-			}
-		}
-	}
-	//fftw example
-	fftw_complex *in, *out;
-	fftw_plan p;
+using namespace std;
+using namespace cimg_library;
 
-	in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * 8);
-	out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * 8);
-	p = fftw_plan_dft_1d(8, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-
-	fftw_execute(p);
-
-	fftw_destroy_plan(p);
-	fftw_free(in); fftw_free(out);
-
-	while(!main_disp.is_closed)
-		main_disp.wait();
-
-	return 0;
-}
-CImg<unsigned char> RGBtoGrayScale(CImg<unsigned char> original_image)
-{
-	CImg<unsigned char> imgGray = original_image.get_RGBtoYCbCr().get_channel(0);
-	imgGray.save("hyojoo_gray.jpeg");
-	return imgGray;
-}
+CImg<unsigned char> RGBtoGrayScale(CImg<unsigned char> original_image);
