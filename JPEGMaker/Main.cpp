6e8b6940536c2dcb5bbc0b7b979200d781dc7dad
parkkj <kjpark@uclab.re.kr>
2015-11-09 01:33:09 +0900

    modify process

--- JPEGMaker/Main.cpp
+++ JPEGMaker/Main.cpp
@@ -1,35 +1,52 @@
-#include "CImg\CImg.h"
+#include <stdio.h>
 #include <Magick++.h>
+
+#include "CImg\CImg.h"
+#include "jpeg\jpeglib.h"
+#include <setjmp.h>
 
 using namespace cimg_library;
 using namespace Magick;
 
+CImg<unsigned char> RGBtoGrayScale(CImg<unsigned char> original);
+
 int main()
 {
-	/*
-	CImg<unsigned char> image("hyojoo.jpeg"), visu(500, 400, 1, 3, 0);
-	const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-	image.blur(2.5);
-	CImgDisplay main_disp(image, "Click a point"), draw_disp(visu, "Intensity profile");
-	
-	while (!main_disp.is_closed() && !draw_disp.is_closed()) {
-		main_disp.wait();	
-		if (main_disp.button() && main_disp.mouse_y() >= 0) {
-			const int y = main_disp.mouse_y();
-			visu.fill(0).draw_graph(image.get_crop(0, y, 0, 0, image.width() - 1, y, 0, 0), red, 1, 1, 0, 255, 0);
-			visu.draw_graph(image.get_crop(0, y, 0, 1, image.width() - 1, y, 0, 1), green, 1, 1, 0, 255, 0);
-			visu.draw_graph(image.get_crop(0, y, 0, 2, image.width() - 1, y, 0, 2), blue, 1, 1, 0, 255, 0).display(draw_disp);
 
-		}
-	}
-	*/
+	/* Change Original image to GrayScale image
 	CImg<unsigned char> imgOriginal("hyojoo.jpeg");
-	CImg<unsigned char> imgGray;
+	CImg<unsigned char> imgGrayScale = RGBtoGrayScale(imgOriginal);
+	*/
+	JSAMPLE *image_buffer;
+	int image_height;
+	int image_width;
 
-	imgGray = imgOriginal.get_RGBtoYCbCr().get_channel(0);
-	imgGray.save("hyojoo_gray.jpeg");
-	CImgDisplay main_disp(imgOriginal, "Main Picture"), sub_disp1(imgGray, "Sub Picture");
+	
+	
 	system("pause");
 	return 0;
 }
+CImg<unsigned char> RGBtoGrayScale(CImg<unsigned char> original)
+{
+	CImg<unsigned char> imgGray;
 
+	imgGray = original.get_RGBtoYCbCr().get_channel(0);
+	imgGray.save("hyojoo_gray.jpeg");
+	return imgGray;
+}
+void write_JPEG_file(char *filename, int quality)
+{
+	struct jpeg_compress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+
+	FILE *outfile;
+	JSAMPROW row_pointer[1];
+	int row_stride;
+
+	cinfo.err = jpeg_std_error(&jerr);
+	jpeg_create_compress(&cinfo);
+
+
+
+
+}
